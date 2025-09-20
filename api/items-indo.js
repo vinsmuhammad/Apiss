af1d8ce790dc438df69621b5b28ebd088dc8d9a2
vinsmuhammad <99631718+vinsmuhammad@users.noreply.github.com>
2025-09-20 15:50:45 +0700

    Update items-indo.js

--- api/items-indo.js
+++ api/items-indo.js
@@ -1,12 +1,15 @@
-import { getItemByIdWithFallbackIndo } from "../scrapers/item_indo.js";
+import { getItemIndoById } from "../scrape/item_indo.js";
 
 export default async function handler(req, res) {
   const { id } = req.query;
-  if (!id) return res.status(400).json({ error: "Missing id" });
+  if (!id) {
+    return res.status(400).json({ error: "Missing id" });
+  }
 
-  const item = await getItemByIdWithFallbackIndo(id, 30);
-  if (!item) return res.status(404).json({ error: "Not found" });
+  const item = await getItemIndoById(Number(id));
+  if (!item) {
+    return res.status(404).json({ error: "Not found" });
+  }
 
   res.status(200).json(item);
 }
-
